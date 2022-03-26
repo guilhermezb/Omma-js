a7b918a262ca9915da988436ae93ff3c79588770
Guilherme Zonca <guiz.48@gmail.com>
2022-03-25 23:02:18 -0300

    finalização

--- index.js
+++ index.js
@@ -34,23 +34,57 @@ const cadastrarReceita = (
   console.log(`Cadastro da receita ${titulo} feito com sucesso!`);
 };
 
-function exibirReceitas() {
-  for (let index = 0; index < listaDeReceitas.length; index++) {
-    const receita = listaDeReceitas[index];
-    console.log("--------------------------------");
-    console.log("Titulo = " + receita.titulo);
+const btnEnviar = document.querySelector('#btnEnviar');
 
-    for (let index = 0; index < receita.ingredientes.length; index++) {
-      const ingrediente = receita.ingredientes[index];
-      console.log("ingredientes = " + ingrediente);
-    }
+btnEnviar.onclick = (evento) => {
+  // impede que a pagina seja recarregada / previne comportamento padrão
+  evento.preventDefault();
 
-    console.log("É vegano? " + receita.vegano);
-    console.log("--------------------------------");
+  // alert('formulario enviado!');
+
+  // selecionando campos do formulário
+  let inputTitulo = document.querySelector('#titulo');
+  let inputDificuldade = document.querySelector('#dificuldade');
+  let inputIngredientes = document.querySelector('#ingredientes');
+  let inputPreparo = document.querySelector('#preparo');
+  let inputLink = document.querySelector('#link');
+  let inputVegano = document.querySelector('input[name="vegano"]:checked');
+
+  let novaReceita = {
+    id: listaDeReceitas.length,
+    titulo: inputTitulo.value,
+    dificuldade: inputDificuldade.value,
+    ingredientes: inputIngredientes.value.split(','),
+    preparo: inputPreparo.value,
+    link: inputLink.value,
+    // vegano: inputVegano.value ? InputVegano.value : 'não'
   }
+
+  console.log(novaReceita);
+  listaDeReceitas.push(novaReceita);
+
+  exibirReceitas();
 }
 
-// exibirReceitas();
+function exibirReceitas() {
+
+  let htmlReceitas = '';
+
+  for (let index = 0; index < listaDeReceitas.length; index++) {
+    htmlReceitas += `<article class="card">
+          <h2>${listaDeReceitas[index].titulo}</h2>
+          <small>Dificuldade: ${listaDeReceitas[index].dificuldade}</small>
+          <p> ${listaDeReceitas[index].preparo}</p>
+      </article>`;
+  }
+
+  let painelReceitas = document.querySelector('.painel-receitas');
+
+  painelReceitas.innerHTML = htmlReceitas;
+
+}
+
+exibirReceitas();
 
 function deletarReceita(id) {
   let novaListaDeReceitas = [];
